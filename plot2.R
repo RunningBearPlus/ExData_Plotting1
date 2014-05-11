d2f0738d864d06628ed88e0d560d15ead936c5df
Claire <clairexqy@gmail.com>
2014-05-11 10:29:20 -0700

    update with transparent backgroud
    white background early, change to transparent in order to match the
    reference files.

--- plot2.R
+++ plot2.R
@@ -4,7 +4,7 @@ b<- subset(a,a$Date == "1/2/2007"|a$Date == "2/2/2007")
 
 b$datetime <-strptime(paste(b$Date,b$Time),"%d/%m/%Y %H:%M:%S")
 
-png(file="plot2.png",width=480,height=480)
+png(file="plot2.png",width=480,height=480,bg = "transparent")
 
 with(b,plot(b$datetime,as.numeric(as.character(b$Global_active_power)),xlab = ""
             ,ylab = "Global Active Power (kilowatts)",type = "l"))
--- plot3.R
+++ plot3.R
@@ -4,7 +4,7 @@ b<- subset(a,a$Date == "1/2/2007"|a$Date == "2/2/2007")
 
 b$datetime <-strptime(paste(b$Date,b$Time),"%d/%m/%Y %H:%M:%S")
 
-png(file="plot3.png",width=480,height=480)
+png(file="plot3.png",width=480,height=480,bg = "transparent")
 
 with(b,plot(b$datetime,as.numeric(as.character(b$Sub_metering_1)),xlab = ""
             ,ylab = "Energy sub-metering",type = "l"))
